43b233f06d23f388fc9f417658ddbc431d7bae2d
Gabrijel Zovkic <gzovkic@1-A-20.42heilbronn.de>
2025-02-18 16:10:32 +0100

    done hopfully

--- push_swap.c
+++ push_swap.c
@@ -6,24 +6,24 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:24:44 by gzovkic           #+#    #+#             */
-/*   Updated: 2025/02/18 15:55:09 by gzovkic          ###   ########.fr       */
+/*   Updated: 2025/02/18 16:10:15 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	leaks(void)
-{
-	printf("\n");
-	system("leaks push_swap");
-}
+// void	leaks(void)
+// {
+// 	printf("\n");
+// 	system("leaks push_swap");
+// }
+// atexit(leaks);
 
 int	main(int argc, char *argv[])
 {
 	char		*arg_str;
 	t_stacks	stacks;
 
-	atexit(leaks);
 	if (argc == 2)
 	{
 		arg_str = ft_strdup(argv[1]);
